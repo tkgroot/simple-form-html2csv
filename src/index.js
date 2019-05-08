1a202cb6cc8ff72a2e28b2115c482bd4f7e96cdd
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-05-08 19:24:38 +0200

    add submit form handler
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+require("bootstrap")
 require("./styles.sass")
+require("jquery-validation")
+
+const form = $("#submitForm")
+
+form.submit((e) => {
+  e.preventDefault()
+
+  // if the form is invalid add the was-validated class and return
+  if (!form.valid()) {
+    form.addClass("was-validated")
+    e.stopPropagation()
+    return undefined
+  }
+  form.removeClass("was-validated")
+
+  // submit formData to the form.php
+  // if successful reset form
+  $.post("form.php", form.serialize(), (data) => {
+    console.log(data)
+    form[0].reset()
+  })
+})
