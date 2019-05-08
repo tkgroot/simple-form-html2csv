cf7e0c73b8d6f02ffb818ca5c045691bf705cb53
Tobias Kuppens Groot <tkuppensgroo@uos.de>
2019-05-08 19:24:23 +0200

    add handling for post request data
    + add check for server request method
    + add stringify util function
    + add validate util function
    + add write_to_file util function
    + add response code and message
    
    Signed-off-by: Tobias Kuppens Groot <tkuppensgroo@uos.de>

---
+++ src/form.php
@@ -0,0 +1,41 @@
+<?php
+
+$CSV_FILE = "signups.csv";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $firstname = validate($_POST["firstname"]);
+    $lastname = validate($_POST["lastname"]);
+    $location = validate($_POST["location"]);
+    $department = validate($_POST["department"]);
+    $short = validate($_POST["short"]);
+    $attendance = validate($_POST["attendance"]);
+
+    write_to_file(stringify($firstname, $lastname, $location, $department, $short, $attendance));
+}
+
+function stringify() {
+    $args = func_get_args();
+    $string_list = "";
+
+    foreach ($args as $item => $value) {
+        $string_list .=$value.",";
+    }
+    return $string_list;
+}
+
+function validate($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+function write_to_file($csv_line) {
+    $file = fopen("signups.csv", "a");
+    fwrite($file, $csv_line . "\n");
+    fclose($file);
+}
+
+http_response_code(200);
+echo "Done"
+?>
